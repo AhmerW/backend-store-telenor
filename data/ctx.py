fe3ec8ad3ac27e4ea1df59ce07db1234a04c049c
Ahmer <63541231+AhmerW@users.noreply.github.com>
2024-10-17 18:29:19 +0200

    cors

--- data/ctx.py
+++ data/ctx.py
@@ -6,7 +6,26 @@ from core.settings import APP_SETTINGS
 from core.responses import Success
 
 
+from fastapi.middleware.cors import CORSMiddleware
 app: Final[FastAPI] = FastAPI(
     title=APP_SETTINGS.PROJECT_NAME,
     default_response_class=Success,
 )
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
